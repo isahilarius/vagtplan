f39fb8f8c692ee153d075bbad33cf096ac0a9173
“isab554b” <isab554b@stud.kea.dk>
2025-10-10 14:25:11 +0200

    tailwind bug

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  mode: "jit",
+  content: ["./*.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        darkblue: "#1E3A8A",
+        darkerblue: "#1B365D",
+        orange: "#F97316",
+      },
+      fontFamily: {
+        flama: "'Flama', sans-serif",
+      },
+    },
+  },
+  plugins: [],
+};
